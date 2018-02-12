6e9a35d05f9d44894e384f47a2840b91b1ff0afa
juanc970410 <juan.c.herrerav@hotmail.com>
2018-02-11 19:24:21 -0500

    modal ended

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import{Component}from'@angular/core';
 import {FormGroup, FormBuilder} from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,12 @@ import {FormGroup, FormBuilder} from '@angular/forms';
 })
 export class AppComponent {
   title = 'app';
+  private navForm:FormGroup;
+  private searchInput:string='';
+  private modalBody:string;
+    constructor (private modalService:NgbModal){}
+
+    search(modalSearch){
+        this.modalService.open(modalSearch);
+    }
 }
