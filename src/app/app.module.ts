40e5a4d824f8a2c52c5f3d2dcb869c716897b799
juanc970410 <juan.c.herrerav@hotmail.com>
2018-02-11 20:54:56 -0500

    performing userListService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,24 @@ import {HomePageComponent}from './pages/home-page/home-page.component';
 import {TaskListPageComponent}from './pages/task-list-page/task-list-page.component';
 import { TaskEditPageComponent}from './pages/task-edit-page/task-edit-page.component';
 import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
+import {UserListPageComponent}from './pages/user-list-page/user-list-page.component';
 
 import {NgbModule}from '@ng-bootstrap/ng-bootstrap';
 
+import { AppConfiguration } from './common/config/app-configuration.service';
+import { HttpModule } from '@angular/http';
+import { INITIAL_CONFIG } from './common/config/initial-config';
+import { AuthService } from './common/auth.service';
+import { AppDataService } from './common/app-data.service';
+
 import {TodoService}from './services/todo.service';
+import {UserService}from './services/users.service';
 
 const ROUTES = [
 {path: '', component: HomePageComponent},
 {path: 'tasks', component: TaskListPageComponent},
 {path: 'edit', component: TaskEditPageComponent },
+{path: 'users', component: UserListPageComponent },
 {path: '**', component: PageNotFoundComponent}]
 
 @NgModule({
@@ -26,7 +35,8 @@ const ROUTES = [
     HomePageComponent,
     TaskListPageComponent,
     TaskEditPageComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    UserListPageComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +45,19 @@ const ROUTES = [
     ReactiveFormsModule,
     FormsModule
   ],
-  providers: [TodoService],
+  providers: [
+    {
+      provide: INITIAL_CONFIG,
+      useValue: {
+        apiURL: 'http://localhost:8080'
+      }
+    },
+    TodoService,
+    AppConfiguration,
+    AuthService,
+    AppDataService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from'@angular/core';
+import 'rxjs/add/observable/of';
+import { Observable } from 'rxjs/Observable';
+import { APIService } from '../common/api.service';
+import { AppConfiguration } from '../common/config/app-configuration.service';
+import { Http } from '@angular/http';
+import { AuthService } from '../common/auth.service';
+import { User } from '../models/user';
+
+@Injectable()
+export class UserService{
+    private users: User[] =[
+    new User ("Bill","Gates","https://pbs.twimg.com/profile_images/929933611754708992/ioSgz49P_400x400.jpg"),
+    new User ("Mark","Zuckerberg","https://specials-images.forbesimg.com/imageserve/59d5062131358e542c034eb7/416x416.jpg?background=000000&cropX1=419&cropX2=1409&cropY1=53&cropY2=1044")];
+
+    constructor(){}
+
+    list(): User[] {
+    return this.users;
+  }
+
+}
